676cd1209e51c2a1975190ca71ebfb4189c7c0e3
Paradise Techosft Solutions <39157936+paradisetechsoftsolutions@users.noreply.github.com>
2020-08-01 16:02:24 +0530

    Update urls.py

--- yolo_app/urls.py
+++ yolo_app/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 # urls are routed here based on logic written on the specific views.
 urlpatterns = [
-	path('', views.index, name='index'),
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('yolo_app', views.yolo_detection_App, name='yolo_app'),
-    path('user_history', views.user_details, name='User_History'),
-    path('logout', views.logout_user, name ='User_Logout')
-]
+	      path('', views.index, name='index'),
+              path('signup/', views.signup_view, name='signup'),
+              path('login/', views.login_view, name='login'),
+              path('yolo_app', views.yolo_detection_App, name='yolo_app'),
+              path('user_history', views.user_details, name='User_History'),
+              path('logout', views.logout_user, name ='User_Logout')
+              ]
 
